ab97dd96a5d086d16bc4f7f5c407c4fa7a367872
e601201 <k.arthur1111@gmail.com>
2022-06-06 06:31:14 +0900

    "[Add]avator_controll_system"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      resource :avators, only: %i[destroy]
+    end
+  end
   root 'cards#index'
   resources :cards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
